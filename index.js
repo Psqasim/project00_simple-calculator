10175f508c3a629e3c21b8c28e97a1265429dcc3
Psqasim <muhammadqasim0326@gmail.com>
2024-03-20 16:54:50 +0500

    change npm command

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([{ message: "enter first number", type: "number", name: "firstnumber" }, { message: "enter second number", type: "number", name: "secondnumber" }, { message: "select one of the operators to perform opetation", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division"], },]);
 //condiional statement
